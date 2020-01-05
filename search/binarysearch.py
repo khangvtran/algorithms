8f09a748575a40bb686253c5a0492ca370b2c625
Khang Vinh Tran <khangtran@Khangs-MacBook-Pro.local>
2020-01-05 09:36:11 -0800

    non recursive binary search

--- search/binarysearch.py
+++ search/binarysearch.py
@@ -1,9 +1,13 @@
 def binary_search(l, left, right, key):
 	'''
 	* desc:
-		this function recursively search for a value in sorted list
+		this function RECURSIVELY search for a value in sorted list
 		It compares the current value at mid index,
 		and adjust the lef and right index appropriately
+		
+		Best Case: O(1) the key is at the mid index of the list
+		Worst case: O(log(n)) the key is at the two ends
+		Average: O(log(n))
 	* args:
 		l <list>: sorted list of values
 		left <int>: left index
@@ -16,7 +20,7 @@ def binary_search(l, left, right, key):
 	# base case
 	if left > right:
 		return(-1)
-		
+
 	# recursion case
 	mid = int((left + right)/2)
 	if l[mid] == key:
@@ -26,3 +30,43 @@ def binary_search(l, left, right, key):
 	else:
 		right = mid - 1
 	return(binary_search(l, left, right, key))
+
+
+def binary_search_iteration(l, key):
+	'''
+	* desc:
+		this function ITERATIVELY search for a value in sorted list
+		It compares the current value at mid index,
+		and adjust the lef and right index appropriately
+		
+		Best Case: O(1) the key is at the mid index of the list
+		Worst case: O(log(n)) the key is at the two ends
+		Average: O(log(n))
+	* args:
+		l <list>: sorted list of values
+		left <int>: left index
+		right <int>: right index
+		key <data type of list element>: the searched value
+	* return:
+		mid <int>: index of key if found. -1 otherwise
+	'''
+	left = 0
+	right = len(l) - 1
+	while left <= right:
+		mid = int((left+right)/2)
+		if l[mid] == key:
+			return(mid)
+		elif l[mid] < key:
+			left = mid + 1
+		else:
+			right = mid - 1
+	return(-1)
+
+
+
+
+
+
+
+
+	
--- search/test.py
+++ search/test.py
@@ -22,3 +22,24 @@ if __name__ == '__main__':
 	print(f'search for {key}: ', end = '')
 	print(binary_search(l, 0, len(l)-1, key))
 
+
+	print('\n### Test Recursion Iteration ###')
+
+	l = [4, 5, 8, 9, 10, 15, 17]
+	print(f'list: {l}')
+
+	key = 15
+	print(f'search for {key}: ', end = '')
+	print(binary_search_iteration(l, key))
+
+	key = 4
+	print(f'search for {key}: ', end = '')
+	print(binary_search_iteration(l, key))
+
+	key = 17
+	print(f'search for {key}: ', end = '')
+	print(binary_search_iteration(l, key))
+
+	key = 6
+	print(f'search for {key}: ', end = '')
+	print(binary_search_iteration(l, key))
